8fe2ce57c595adeb9f6b89b7506f580b7dc10d90
Arthur Cesarino <contatocesarino@gmail.com>
2020-03-19 11:27:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,6 +11,8 @@ public class Program {
 
         Department department = new Department(1, "Books");
 
-        System.out.println(department);
+        Seller seller = new Seller(21, "Arthur", "art@gmail.com", new Date(), 3000.00, department);
+
+        System.out.println(seller);
     }
 }
